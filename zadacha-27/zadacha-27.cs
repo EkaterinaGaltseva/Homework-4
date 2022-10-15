720c7445cc38eaf82c7bc8d9886d8e5b62a23be6
Galtseva Ekaterina <katerina.vedernikova-95@mail.ru>
2022-10-15 12:12:53 +0500

    решение четвертой домашней работы

---
+++ zadacha-27/zadacha-27.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+Console.Write("Введите число N: ");
+string NumberN = Console.ReadLine();
+int NumN = int.Parse(NumberN);
+int summ = 0;
+while (NumN > 0)
+{
+    summ = summ + NumN % 10;
+    NumN = NumN / 10; 
+
+}
+Console.Write($"{NumberN} -> {summ} ");
---
+++ zadacha-29/Program.cs
@@ -0,0 +1,35 @@
+﻿/*  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())*/
+Console.Write("Введите цифры массива через запятую ");
+string arr = Console.ReadLine();
+string [] ArrayTwo = arr.Split(',');
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+GetArray(size);
+
+
+if (ArrayTwo.Length != size)
+{
+    Console.Write($"Массив не из {size} символов");
+    return;
+}
+
+void GetArray(int array)
+{
+   
+    int[] MyArray = new int[array];
+    for (int i = 0; i < ArrayTwo.Length; i++)
+    {
+        MyArray[i] = Int32.Parse(ArrayTwo[i]);
+        
+    }
+    Console.Write("[{0}]", string.Join(",", MyArray));
+}
+
+
